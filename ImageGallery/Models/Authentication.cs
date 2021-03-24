99b93860fc886d27dfe646faf755df706ccc98a1
JZunigaV <j_zuniga4@hotmail.com>
2021-03-23 23:55:56 -0600

    Added first changes

---
+++ ImageGallery/Models/Authentication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Models
+{
+    public class Authentication
+    {
+
+        public bool Auth { get; set; }
+
+        public string Token { get; set; }
+
+    }
+}
---
+++ ImageGallery/Models/Image.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Models
+{
+    public class Image
+    {
+
+        public string Id { get; set; }
+        
+        public string Author { get; set; }
+
+        public string Camera { get; set; }
+
+        public string Tags { get; set; }
+
+        [JsonProperty("cropped_picture")]
+        public string CroppedPicture { get; set; }
+
+        [JsonProperty("full_picture")]
+        public string FullPicture { get; set; }
+    }
+}
